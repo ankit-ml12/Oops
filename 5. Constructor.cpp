e0f9521ac314bd310efaf4efd8bc6b1fc89544d6
ankit patidar <patiankit6555@gmail.com>
2022-12-21 08:36:08 +0530

    1oo

---
+++ 5. Constructor.cpp
@@ -0,0 +1,42 @@
+#include <bits/c++.h>
+using namespace std;
+
+class Student{
+  public:
+  int age;
+  int rollNumber;
+ 
+    // default constructor
+    // student(){
+    // }
+    // the movement we create constructor default constructor will not work
+    
+    // Student(int age){
+        // this.age= age;
+    // }
+    // Student(int age, int rollNumber){
+        // this.age= age;
+        // this.rollNumber= rollNumber
+    // }
+    
+    
+    // this constructor change the default value of object 
+    // if you create multiple constructor with different no of variable 
+    // only which constructor will call who has no of variable equal to no of argument
+    
+};
+
+int main(){
+    
+    // whenever we call a student function to create a object like
+    Student s1;
+    // when we create it call automatically to s1.Student();
+    //a bidefault constructor will run which assign the default value property of class to the student
+    //property of default constructor
+    //same name as a class 
+    //no return type
+    //no input argument
+    
+    
+    return 0;
+}
