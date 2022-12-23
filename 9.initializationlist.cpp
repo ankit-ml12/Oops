ea2a981f5b363080e1b179ff0efd091732cdb6ec
ankit patidar <patiankit6555@gmail.com>
2022-12-23 08:08:34 +0530

    1html

---
+++ 9.initializationlist.cpp
@@ -0,0 +1,27 @@
+// we can not declare constant and reference variable after declaring them
+// now let suppose if we want to make some propety constant then how to do that
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Students{
+    const int rollnu;
+    int age;
+    int &x;
+    // we initilize like this to assign some value to const
+    //here we can't write age because is is clearly visible outer is for main
+    // in innner is for parameter
+    Students(int r, int age): rollnu(r), age(age), x(this->age){
+        
+    }
+    
+    // this is call initilizeation list we have to user for const and reference variable
+};
+
+int main(){
+    
+    
+    
+    return 0;
+}
