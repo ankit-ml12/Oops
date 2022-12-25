fce41854f0fe22da3e0fa221fa376c0cafd65c8c
ankit patidar <patiankit6555@gmail.com>
2022-12-25 10:44:57 +0530

    4cpp

---
+++ 17. inheritenceex.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Vehical{
+    private:
+    int maxSpeed;
+    protected:
+    int numTyres;
+    public:
+    string color;
+};
+
+class Car : public Vehical{
+    public:
+    int numGears;
+    
+    
+    // above numTyres and color accessible here
+}
+
+//if we make sub class public class of this car they can also able to access
+// punlic and protected into sub class of base class and parent class
+
+
+class Car2 : protected Vehical{
+    public:
+    int numGears;
+    
+    
+    // above numTyres and color accessible here but become protected property of this class 
+}
+class Car3 : private Vehical{
+    public:
+    int numGears;
+    
+    
+    // above numTyres and color accessible here but become private property of this class 
+}
+
+
+int main(){
+    //here we can only access public function not to the private function
+    
+    
+    return 0;
+}
