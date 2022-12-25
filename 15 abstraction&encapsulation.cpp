4df40ac189354ffb359d11eec9f348070d4d3de3
ankit patidar <patiankit6555@gmail.com>
2022-12-25 10:24:59 +0530

    4cpp

---
+++ 15 abstraction&encapsulation.cpp
@@ -0,0 +1,7 @@
+// Encapsultion:-  clubbing or combining
+// Encapsulation  is a process of wrapping data and function together into a single unit, for example, a capsule which is mixed of several medicines.
+
+
+// abstraction:- Abstraction is the process of hiding the internal details of an application from the outer world. Abstraction is used to describe things in simple terms. It’s used to create a boundary between the application and the client programs.
+
+// ex:- car, sortfunction in cpp, STL
---
+++ 16 inheritense.cpp
@@ -0,0 +1,9 @@
+// Inheritance in cpp is a mechanism in which one object acquires all the properties and behaviors of a parent object. It is an important part of OOPs
+// (Object Oriented programming system).
+
+// The idea behind inheritance in cpp is that you can create new classes
+// that are built upon existing classes. When you inherit from an existing class, you can reuse methods and fields of the parent class. Moreover, you can add new methods and fields in your current class also.
+
+// Heere we use protected access modifier
+// when we declare some propety as pretected it means this property is accessible only to my child class
+
