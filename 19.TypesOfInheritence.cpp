32c6315e59d5a88c3f1bdebee64e0ee05cd7c143
ankit patidar <patiankit6555@gmail.com>
2022-12-26 13:05:29 +0530

    5cpp

--- 19.TypesOfInheritence.cpp
+++ 19.TypesOfInheritence.cpp
@@ -1,23 +1,36 @@
-// 1. Single inheritance
-// where only one parents and one derieved class 
-// 2. Multilevel inheritance
-// multiple level of child and parents 
-// 3. hierarical inheritance 
-// one parent class is inherited by multiple derieved class 
+// // 1. Single inheritance
+// // where only one parents and one derieved class 
+// // 2. Multilevel inheritance
+// // multiple level of child and parents 
+// // 3. hierarical inheritance 
+// // one parent class is inherited by multiple derieved class 
 
-// 4. multiple inherience 
-// multiple parent class is inherited by same class 
+// // 4. multiple inherience 
+// // multiple parent class is inherited by same class 
 
-// ex:- 
-// class TA :public teacher, public student{
+// // ex:- 
+// // class TA :public teacher, public student{
 
-// }
-// in above case first teacher constructor will call and then student and then TA constructor 
+// // }
+// // in above case first teacher constructor will call and then student and then TA constructor 
 
-// if print function present in both teacher and student what will happen
+// // if print function present in both teacher and student what will happen
 
-// we have to specify like
-// TA a;
-// a.student :: print();
-// for TA class we can directly call it without resolution
+// // we have to specify like
+// // TA a;
+// // a.student :: print();
+// // for TA class we can directly call it without resolution
+
+// 5. Hybrid inheritence 
+// // Ex;- 
+// // A      B                       A 
+// //  \    /                       / \
+// //   \  /                       /   \
+// //    C                        B     C
+// //   / \                        \   /
+// //  /   \                        \ /
+// // D     E                        D   
+
+// // Some time d contain function from b and c both which cause problem
+// // this problem is called diamond problem
 
