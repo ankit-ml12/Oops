9965a8aabed46d279789a35288c2937df913b6a6
ankit patidar <patiankit6555@gmail.com>
2022-12-27 09:03:57 +0530

    5cpp

---
+++ 21.polymorphism.cpp
@@ -0,0 +1,25 @@
+// polymorphism means :- many form
+
+// types:- 
+// Compile time 
+// Run time 
+// means we decide on the compile time or run time in which way we have to behave
+
+// Compile time:- 
+// 1. Can be achive by function overloading and operator overloading
+
+// Function overloading
+int test (int a, int b){ 
+}
+int test (int a){ 
+}
+int test (){ 
+}
+// for operator overloading follow 12 file 
+
+// 2. function overriding
+// example of it if both parent and child class have print function and when we call the child function then the consturctor of both the function will call but in case of print function only child print function will call because we are overriding the parent print with child print  
+ 
+
+
+//  Run time polymorphism
